a9a4eb779305ffe56c1b2a387fad78451d5e1c88
zztkm <zztkm@veltiosoft.com>
2025-11-08 13:04:21 +0900

    ping の返答を初代と似た感じにする

--- src/commands/ping.rs
+++ src/commands/ping.rs
@@ -2,7 +2,7 @@ use serenity::builder::CreateCommand;
 use serenity::model::application::ResolvedOption;
 
 pub fn run(_options: &[ResolvedOption]) -> String {
-    "二代目タコ八です！".to_string()
+    ":ping_pong: pong !".to_string()
 }
 
 pub fn register() -> CreateCommand {
